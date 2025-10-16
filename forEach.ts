7d2309c583ea738327b0cd0a26a7be4145fbfd83
luebbertevan <luebbertevan@gmail.com>
2025-10-16 17:09:56 -0400

    more practice

--- forEach.ts
+++ forEach.ts
@@ -1,19 +1,19 @@
-//.forEach is useful for oneliner clarity like for console.log and single function calls with no breaks or returns
+// //.forEach is useful for oneliner clarity like for console.log and single function calls with no breaks or returns
 
-// Log every number in [10, 20, 30].
-function logAll(nums: number[]) {
-	nums.forEach((w) => console.log(w));
-}
-logAll([10, 20, 30]);
+// // Log every number in [10, 20, 30].
+// function logAll(nums: number[]) {
+// 	nums.forEach((w) => console.log(w));
+// }
+// logAll([10, 20, 30]);
 
-// Write a function countAboveAverage(nums: number[]): number that counts how many numbers are greater than the average.
-function countAboveAverage(nums: number[]): number {
-	let count = 0;
-	const avg = nums.reduce((a, c) => a + c, 0) / nums.length;
+// // Write a function countAboveAverage(nums: number[]): number that counts how many numbers are greater than the average.
+// function countAboveAverage(nums: number[]): number {
+// 	let count = 0;
+// 	const avg = nums.reduce((a, c) => a + c, 0) / nums.length;
 
-	nums.forEach((n) => {
-		if (n > avg) count++;
-	});
+// 	nums.forEach((n) => {
+// 		if (n > avg) count++;
+// 	});
 
-	return count;
-}
+// 	return count;
+// }
