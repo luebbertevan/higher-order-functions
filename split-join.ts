237c85c303dcd523b7ad70bf7ff0c0950a702a8a
luebbertevan <luebbertevan@gmail.com>
2025-10-16 11:32:30 -0400

    split and join

---
+++ split-join.ts
@@ -0,0 +1,9 @@
+const in1 = "apple, banana, cherry, date ";
+
+console.log(in1.split(",").map((w) => w.trim()));
+
+const in2 = "2025-10-15-13-45";
+
+const parts = in2.split("-");
+const result = [...parts.slice(0, 2), parts.slice(2).join("-")];
+console.log(result);
